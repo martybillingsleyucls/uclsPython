beb5901a069f38fb2f2cc54d044588acaf534908
martybillingsleyucls <marty@ucls.uchicago.edu>
2020-07-08 22:10:20 -0500

    Create draw.py

---
+++ turtleDraw/draw.py
@@ -0,0 +1,15 @@
+import turtle
+
+# create a graphic window for the turtle to draw on
+win = turtle.Screen()
+# create a turtle named bob
+alex = turtle.Turtle()
+
+
+
+
+
+# put your code above this line
+# this command is needed to make the GUI window work properly 
+# and should always be the last line in the program
+turtle.done()
