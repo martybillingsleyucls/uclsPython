c36651de310412e2f84450d79dbc6226810142bd
martybillingsleyucls <marty@ucls.uchicago.edu>
2020-07-08 19:40:44 -0500

    Create tree.py

---
+++ fractalTrees/tree.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+# define your drawTree() function here
+
+
+
+
+
+
+# move the turtle to a good starting position
+penup()
+goto(0,-200)
+setheading(90)
+pendown()
+
+# call your drawTree function here
+
+
+
+
+# this line is needed to make the GUI window work properly
+done()
