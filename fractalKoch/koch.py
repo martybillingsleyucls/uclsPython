ae60b738246fb1c3ee96a24afaa63635b8922523
martybillingsleyucls <marty@ucls.uchicago.edu>
2020-07-08 19:55:26 -0500

    Create koch.py

---
+++ fractalKoch/koch.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+# define your drawLineSegment() function here
+
+
+
+
+
+
+# move the turtle to a good starting position
+penup()
+goto(-200,0)
+setheading(0)
+pendown()
+
+# call your function here
+
+
+
+
+# this line is needed to make the GUI window work properly
+done()
